35d9b8a7da7b3efed574f64412387a6b3d175571
salminnella@gmail.com <salminnella@gmail.com>
2018-06-08 15:51:50 -0700

    updated to studio 3.2

---
+++ app/src/main/java/com/demo/salminnella/intentservicesleeper/MainActivity.java
@@ -0,0 +1,35 @@
+package com.demo.salminnella.intentservicesleeper;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
+
+    private Button downloadButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+        setClickListener(downloadButton);
+    }
+
+    private void initViews() {
+        downloadButton = (Button) findViewById(R.id.download_button_main);
+    }
+
+    private void setClickListener(View view) {
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
