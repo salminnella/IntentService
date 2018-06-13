77e5e8a743e63d72634cf7c35bf6c7478265da77
salminnella@gmail.com <salminnella@gmail.com>
2018-06-12 17:01:30 -0700

    added download receiver class

---
+++ app/src/main/java/com/demo/salminnella/intentservicesleeper/DownloadReceiver.java
@@ -0,0 +1,29 @@
+package com.demo.salminnella.intentservicesleeper;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.ResultReceiver;
+
+
+public class DownloadReceiver extends ResultReceiver {
+    private Receiver mReceiver;
+
+    public DownloadReceiver(Handler handler) {
+        super(handler);
+    }
+
+    public void setReceiver(Receiver receiver) {
+        mReceiver = receiver;
+    }
+
+    public interface Receiver {
+        public void onReceiveResult(int resultCode, Bundle resultData);
+    }
+
+    @Override
+    protected void onReceiveResult(int resultCode, Bundle resultData) {
+        if (mReceiver != null) {
+            mReceiver.onReceiveResult(resultCode, resultData);
+        }
+    }
+}
